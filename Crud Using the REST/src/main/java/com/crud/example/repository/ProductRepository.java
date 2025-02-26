0d4c4708045db303e91e3f486163726c2354059b
DhruvParmar1402 <parmardhruv924@gmail.com>
2025-02-26 18:44:24 +0530

    Added REST crud example

---
+++ Crud Using the REST/src/main/java/com/crud/example/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.crud.example.repository;
+
+import com.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
